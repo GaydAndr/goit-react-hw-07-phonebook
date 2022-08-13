52e3ea8cf321c9479c9287c6d7203992e0d96d41
andrii_h <galgayand@gmail.com>
2022-08-14 01:40:30 +0300

    fix

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,20 +1,20 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { deleteContact, getContacts, postContact } from 'service/contactAPI';
-import { FETCH } from './contacts-types';
+import { ADD, DELETE, FETCH } from './contacts-types';
 
 export const fetchContacts = createAsyncThunk(FETCH, async () => {
   const { data } = await getContacts();
   return data;
 });
 
-export const addContact = createAsyncThunk(FETCH, async contact => {
-  await postContact(contact);
-  const { data } = await getContacts();
+export const addContact = createAsyncThunk(ADD, async contact => {
+  const data = await postContact(contact);
+  console.log(data);
   return data;
 });
 
-export const deleteItem = createAsyncThunk(FETCH, async id => {
-  await deleteContact(id);
-  const { data } = await getContacts();
-  return data;
+export const deleteItem = createAsyncThunk(DELETE, async id => {
+  const data = await deleteContact(id);
+  console.log(data.id);
+  return data.id;
 });
